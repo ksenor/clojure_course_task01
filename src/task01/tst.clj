3898e3a929b340d6b18438ec8d80661398698a50
ks <ks@ks-work.(none)>
2013-03-21 11:01:13 +0400

    f

--- src/task01/tst.clj
+++ src/task01/tst.clj
@@ -2,6 +2,28 @@
   (:require [pl.danieljanus.tagsoup :refer :all])
   (:gen-class))
 
+(defn just-vector [a]
+    (filter vector? a))
+
+
+(defn just-tag
+  [a]
+  (filter
+        (fn [y] (= ':a (first y)))
+        (just-vector a)))
+
+
+(defn just-link [a]
+  (map (fn [y] (:href (second y)))
+       (just-tag a)))
+
+
+(defn vector-data
+  [a]
+  (if (and (= ':h3 (first a))
+  (= (:class (second a)) "r"))
+    (just-link a)
+    (into [] (mapcat vector-data (just-vector a)))))
 
 (defn get-links []
 " 1) Find all elements containing {:class \"r\"}.
@@ -16,12 +38,14 @@ Example:
 
 The link from the example above is 'https://github.com/clojure/clojure'.
 
-3) Return vector of all 10 links.
+  3) Return vector of all 10 links.
 
 Example: ['https://github.com/clojure/clojure', 'http://clojure.com/', . . .]
 "
   (let [data (parse "clojure_google.html")]
-    nil))
+    (vector-data data)))
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
+
+(-main)
